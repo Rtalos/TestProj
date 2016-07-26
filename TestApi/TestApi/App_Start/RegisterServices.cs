62cee4c5f8db4c735626da7d95bec9306644977d
robert <robert.talos@outlook.com>
2016-07-26 13:11:01 +0200

    Updated with unity

---
+++ TestApi/TestApi/App_Start/RegisterServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Practices.Unity;
+using System.Web;
+
+namespace TestApi.App_Start
+{
+    public class RegisterServices
+    {
+        public static void RegisterServiceApplication()
+        {
+            var config = new UnityConfig();
+        }
+    }
+}
